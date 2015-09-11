7213375bce65342b525c54f300f654d74b44832d
Guillaume Dumont <dumont.guillaume@gmail.com>
2015-09-11 16:50:26 -0400

    Implemented reload

--- lib/Reload-Module.ps1
+++ lib/Reload-Module.ps1
@@ -5,6 +5,18 @@ Function Remove-AndImportModule {
             $Options = get-module | % { $_.Name }
             New-DynamicParam -Name Name -ValidateSet $Options -Position 0 -Mandatory
     }
+    begin {
+    #have to manually populate
+    $Name = $PSBoundParameters.Name    
+    }
+    process {
+        $Module = Get-Module $_
+        if($Module) {
+            Remove-Module $_            
+        }
+        Import-Module $_
+    }
+    end {}
 }
 
 New-Alias reload Remove-AndImportModule -Scope Global
